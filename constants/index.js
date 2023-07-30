4674411f4ce9954988c73815664cf77dbcc2b75a
adevil7009 <99535500+adevil7009@users.noreply.github.com>
2023-07-30 18:06:56 +0100

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xbe176083caf3ad9d53af84bfc56b3ed5c671a7b9";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
